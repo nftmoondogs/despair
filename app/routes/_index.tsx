9e519dd1e2ba8c07d8d489e8a21cf4a67ab8fb7d
nftmoondogs <nftmoondogs@gmail.com>
2024-07-02 19:39:24 +0530

    newcommit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -160,7 +160,7 @@ export default function Index() {
     <button className="p-[3px] relative">
         <div className="absolute inset-0 bg-gradient-to-r from-red-800 to-red-300 rounded-lg" />
         <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
-        <Link to="https://pump.fun/EzjPm34sgJAo99cBc45S4U3V1zPqSgqZctHwKM11PAQs">
+        <Link to="https://pump.fun/board">
           Pump.Fun💊
         </Link>
         </div>
