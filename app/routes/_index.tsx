aabd2ed2a040beded8016a6118573706b78b2d24
nftmoondogs <nftmoondogs@gmail.com>
2025-05-05 19:19:32 +0530

    “booppair2”

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -107,7 +107,7 @@ export default function Index() {
             </span>{" "}
             Destruction of {" "}
             <Link
-              to="https://boop.fun/"
+              to="https://boop.fun"
               className="font-extrabold bg-gradient-to-r from-red-800 via-red-300 to-red-50 text-transparent bg-clip-text bg-300% animate-gradient hover:text-emerald-500"
             >
               boop.fun
